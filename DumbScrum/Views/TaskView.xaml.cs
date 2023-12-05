1a55e23dee4dbcef21bb72b748d9ad7ddc137ab3
JayrodHarv <jared.harvey10@gmail.com>
2023-12-05 09:05:22 -0600

    completed the task reviewing logic

--- DumbScrum/Views/TaskView.xaml.cs
+++ DumbScrum/Views/TaskView.xaml.cs
@@ -16,6 +16,7 @@ namespace DumbScrum.Views {
         UserVM user;
         TaskManager taskManager = new TaskManager();
         UserManager userManager = new UserManager();
+        FeedMessageManager feedMessageManager = new FeedMessageManager();
         public TaskView(string projectID, int taskID, UserVM user) {
             this.projectID = projectID;
             this.taskID = taskID;
@@ -30,7 +31,6 @@ namespace DumbScrum.Views {
                 if(task.UserID != 0) { // it's set to 0 if userid is null in the accessor
                     User user = userManager.GetUser(task.UserID);
                     tbUser.Text = user.DisplayName;
-                    btnCommitToTask.Visibility = Visibility.Collapsed;
                 }
                 tbUserStory.Text = task.Story;
                 tbStatus.Text = task.Status;
@@ -40,8 +40,15 @@ namespace DumbScrum.Views {
             switch(task.Status) {
                 case "In Progress":
                     btnSendToBeReviewed.Visibility = Visibility.Visible;
+                    tabReview.Visibility = Visibility.Collapsed;
                     break;
                 case "Needs Reviewed":
+                    btnSendToBeReviewed.Visibility = Visibility.Collapsed;
+                    tabReview.Visibility = Visibility.Visible;
+                    break;
+                case "Complete":
+                    btnSendToBeReviewed.Visibility = Visibility.Collapsed;
+                    tabReview.Visibility = Visibility.Collapsed;
                     break;
             }
 
@@ -53,18 +60,58 @@ namespace DumbScrum.Views {
             tabDM.Content = new FileUploadView(projectID, taskID, "Data Model");
         }
 
-        private void btnCommitToTask_Click(object sender, RoutedEventArgs e) {
-
-        }
-
         private void btnSendToBeReviewed_Click(object sender, RoutedEventArgs e) {
             try {
                 if (taskManager.UpdateTaskStatus(taskID, "Needs Reviewed")) {
+                    MainWindow window = (MainWindow)Window.GetWindow(this);
+                    ProjectView projectView = (ProjectView)window.CurrentView;
+                    projectView.CurrentProjectView = new BoardView(projectID, user);
                     MessageBox.Show("Success!");
                 }
             } catch (Exception ex) {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private void btnSubmitReview_Click(object sender, RoutedEventArgs e) {
+            if(user.UserID == task.UserID) {
+                MessageBox.Show("You can't review your own work, it defeats the purpose of reviewing things.");
+                return;
+            }
+            if (tbReviewNotes.Text == "") {
+                MessageBox.Show("Please provide at least some message for the review.");
+                return;
+            }
+            if (cbUseCase.IsChecked == true &&
+                cbSPS.IsChecked == true &&
+                cbUserInterface.IsChecked == true &&
+                cbERD.IsChecked == true &&
+                cbDD.IsChecked == true &&
+                cbDM.IsChecked == true) {
+                // all of the work looked good to the reviewer | change task status to complete
+                if (taskManager.UpdateTaskStatus(taskID, "Complete")) {
+                    SumbitReview();
+                }
+            } else {
+                if (taskManager.UpdateTaskStatus(taskID, "In Progress")) {
+                    SumbitReview();
+                }
+            }
+        }
+
+        private void SumbitReview() {
+            string messageText = "Review of task " + taskID + ":\n" + tbReviewNotes.Text;
+            FeedMessage message = new FeedMessage() {
+                SprintID = task.SprintID,
+                UserID = user.UserID,
+                Text = messageText,
+                SentAt = DateTime.Now
+            };
+            feedMessageManager.CreateFeedMessage(message);
+            MainWindow window = (MainWindow)Window.GetWindow(this);
+            ProjectView projectView = (ProjectView)window.CurrentView;
+            projectView.CurrentProjectView = new BoardView(projectID, user);
+            MessageBox.Show("Review successfully submited. Feed message created showing the review message.");
         }
     }
 }
